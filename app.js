0b9aa18c4da0f8c487409d6689ca0ef8ab67465e
Parsa-mh <mahmoodiparsa654@gmail.com>
2024-02-11 15:45:10 +0330

    initial commit

---
+++ app.js
@@ -0,0 +1,131 @@
+//html items
+const label = document.querySelector("#Label-input")
+const inputNumber = document.querySelector("#inputNumber")
+const btn = document.querySelector("#submit")
+const feedback = document.querySelector("#feedback")
+const section = document.querySelector("#section")
+//previous guesses array
+let previousGuesses = []
+
+//random number
+function random (){
+    return Math.round(Math.random()*100)
+}
+const randomNumber = random()
+
+//chanses
+let Counter = 5
+
+function guessNumber (event){
+    event.preventDefault()
+    const userNumber = inputNumber.value
+    //rules
+    if (userNumber == "" ){
+        alert("لطفا فیلد را خالی نگزارید")
+    }
+    else if (userNumber > 100 || userNumber < 1){
+        alert("لطفا اعداد بزرگ تر از 100 و یا کوچک تر از 1 وارد نکنید")
+    }
+    else if (previousGuesses.includes(userNumber)){
+        alert("این مقدار قبلا انتخاب شده")
+    }
+    else if (isNaN (Number (userNumber)) == true){
+        alert("مقدار وارد شده صحیح نمیباشد")
+    }
+    //game
+    else {
+        //win
+        if (randomNumber == userNumber){
+            feedback.textContent = "you won :)"
+            //feedback part
+            if (feedback.className.includes("text-warning")){
+                feedback.classList.replace("text-warning","text-success")
+            }
+            else{
+                feedback.classList.replace("text-danger","text-success")
+            }
+            //disabling game
+            inputNumber.setAttribute("disabled","")
+            btn.setAttribute("disabled","")
+            //styles
+            label.remove()
+            document.querySelector("h5").outerHTML = `<h1 class="text-center text-success mb-5 mt-3 w-100">Congratulations !</h1>`
+            document.querySelector("h6").textContent = "Your guesses"
+            document.querySelector("body").classList.add("bg-success")
+            document.querySelector("#restart").classList.replace("d-none","d-block")
+            document.querySelector("#restart").classList.replace("btn-danger","btn-success")
+            //list of previous guesses
+            const list = `<li class="list-group-item list-group-item-action list-group-item-secondary">
+            ${userNumber}
+            <span class="text-success float-end">Currect</span>
+            </li>`
+            document.querySelector("ul").innerHTML += list
+            section.classList.replace("d-none","d-block")
+            ///////////////////////////////////////////////////////////////
+            inputNumber.value = ""
+            //losing one chance
+            Counter--
+        
+        }
+        //user guess lower
+        else if (randomNumber > userNumber){
+            //feedback part
+            feedback.textContent = "Low"
+            feedback.classList.replace("text-danger","text-warning")
+            //list of previous guesses
+            const list = `<li class="list-group-item list-group-item-action list-group-item-secondary">
+            ${userNumber}
+            <span class="text-warning float-end">${feedback.textContent}</span>
+            </li>`
+            document.querySelector("ul").innerHTML += list
+            section.classList.replace("d-none","d-block")
+
+            inputNumber.value = ""
+            //losing one chance
+            Counter--
+        }
+        //user guess higher
+        else if (randomNumber < userNumber){
+            //feedback part
+            feedback.textContent = "High"
+            feedback.classList.replace("text-warning","text-danger")
+            //list of previous guesses
+            const list = `<li class="list-group-item list-group-item-action list-group-item-secondary">
+            ${userNumber}
+            <span class="text-danger float-end">${feedback.textContent}</span>
+            </li>`
+            document.querySelector("ul").innerHTML += list
+            section.classList.replace("d-none","d-block")
+
+            inputNumber.value = ""
+            //losing one chance
+            Counter--
+        }
+        //error
+        else {
+            alert("مقدار وارد شده صحیح نمیباشد")
+        }
+        //lost
+        if (Counter < 1 && !document.querySelector("body").className.includes("bg-success")){
+            //feedback part
+            feedback.classList.replace("text-warning","text-danger")
+            feedback.textContent = `Number : ${randomNumber}`
+            //disabling game
+            inputNumber.setAttribute("disabled","")
+            btn.setAttribute("disabled","")
+            /////////////////////////////////////////
+            //saying true number
+            document.querySelector("h5").outerHTML = `<h1 class="text-center text-danger mb-5 mt-3 w-100">you Lost:(</h1>`
+            //styles
+            label.remove()
+            document.querySelector("body").classList.add("bg-danger")
+            document.querySelector("h6").textContent = "your guesses"
+            document.querySelector("#restart").classList.replace("d-none","d-block")
+        }
+        previousGuesses.push(userNumber)
+    }
+}
+//restart button 
+function reloadGame (){
+    window.location.reload()
+}
